123b0d77b66bc011e2edb4774145656d545594ca
Sebastian <kvaksebastian@gmail.com>
2021-10-08 10:35:02 +0300

    t2

--- TEST.js
+++ TEST.js
@@ -2,9 +2,6 @@
 2
 43
 // echo "# tewst" >> README.md
-// git init
-// git add README.md
-// git commit -m "first commit"
 // git branch -M main
 // git remote add origin git@github.com:KvakSebastian/tewst.git
 // git push -u origin main
