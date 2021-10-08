b65a1bc585dc840f195824496d86c4c0589fa9df
Sebastian <kvaksebastian@gmail.com>
2021-10-08 13:37:38 +0300

    test 3

--- TEST.js
+++ TEST.js
@@ -10,26 +10,6 @@
 43
 // echo "# tewst" >> README.md
 // git branch -M main
-// git remote add origin git@github.com
-1
-2
-43
-// echo "# tewst" >> README.md
-// git branch -M main
-// git remote add origin git@github.com:KvakSebastian/tewst.git
-// git push -u origin main
-1
-2
-43
-// echo "# tewst" >> README.md
-// git branch -M main
-// git remote add origin git@github.com:KvakSebastian/tewst.git
-// git push -u origin main
-1
-2
-43
-// echo "# tewst" >> README.md
-// git branch -M main
 // git remote add origin git@github.com:KvakSebastian/tewst.git
 // git push -u origin main:KvakSebastian/tewst.git
 // git push -u origin main
